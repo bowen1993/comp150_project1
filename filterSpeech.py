75c51a5ac7b0984cca64eff8cc2da30b8d5d923f
bowen <bowen.g.1993@gmail.com>
2018-10-20 15:02:11 -0400

    filter roles

--- filterSpeech.py
+++ filterSpeech.py
@@ -1,3 +1,32 @@
+import nltk
 import json
 import os
+import csv
+import collections
 
+all_roles = collections.defaultdict(list)
+
+for subdir, dirs, files in os.walk('data'):
+    for filename in files:
+        if filename.endswith('json'):
+            fullname = os.path.join(subdir, filename)
+            data_file = open(fullname, 'r')
+            data = json.load(data_file)
+            filteredData = dict()
+            for role in data:
+                filteredData[role] = filter(lambda x : len(nltk.word_tokenize(x)) < 10, data[role])
+                all_roles[role].extend(filteredData[role])
+            data_file.close()
+            newFile = open(fullname, 'w')
+            json.dump(filteredData, newFile)
+
+print(all_roles)
+
+csv_file = open('name_id.csv')
+filtered_csv = open('filtered_name_id.csv', 'w')
+csv_reader = csv.reader(csv_file)
+csv_writer = csv.writer(filtered_csv)
+
+for row in csv_reader:
+    if row[0] in all_roles and len(all_roles[row[0]]) > 0:
+        csv_writer.writerow(row)
